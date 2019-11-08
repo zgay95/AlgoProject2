8ed94f0e0539333d7f6ffbcacb0d73efaa3648b3
Danori <lilbill1120@aol.com>
2019-11-08 13:56:48 -0500

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,30 +1,58 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
-#include <vector>
 #include <limits>
+#include <fstream>
 
 class Matrix {
+private:
+    static const int sentinelVal = std::numeric_limits<int>::min();
+
+    struct MemoElement {
+        MemoElement() : val(sentinelVal), oldRow(-1), newRow(-1) { }
+        MemoElement(int val) : val(val), oldRow(-1), newRow(-1) { }
+        MemoElement(int val, int oldRow, int newRow) : val(val), oldRow(oldRow), newRow(newRow) { }
+
+        MemoElement& operator=(MemoElement right) {
+            val = right.val;
+            oldRow = right.oldRow;
+            newRow = right.newRow;
+            
+            return *this;
+        }
+
+        bool operator<(MemoElement right) {
+            return val < right.val;
+        }
+
+        int val;
+        int oldRow;
+        int newRow;
+    };
+
 public:
     Matrix();
     ~Matrix();
-    void readInMatrix();
     void writeOutSolution();
-    int memoCalcMaxSum(const int j, const int oldRow, const int newRow);
-    void iterCalcMaxSum();
+    int memoCalcMaxSum(const int &j, const int &oldRow, const int &newRow);
+    int iterCalcMaxSum();
     void printMatrix();
     void printMemo();
+    
 private:
+    void initMatrix(std::ifstream &input);
     void initMemo();
-    const int calcPenalty(const int currRow, const int oldRow, const int newRow);
+    void deleteMatrix();
+    void deleteMemo();
+    const int calcPenalty(const int &currRow, const int &oldRow, const int &newRow);
+
     const size_t index(const int &i, const int &j);
     const size_t index(const int &i, const int &j, const int &k);
 
     int rows = 0;
     int cols = 0;
-    int sentinelVal = std::numeric_limits<int>::min();
     int *matrix = nullptr;
-    int *memo = nullptr;
+    MemoElement *memo = nullptr;
 };
 
 #endif // MATRIX_HPP
